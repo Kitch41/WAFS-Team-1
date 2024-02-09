98692d6426ddc184d6c831a25f23279882fd6933
Kitch41 <stefkeuken@hotmail.com>
2024-02-09 15:19:59 +0100

    final day 5

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,8 +1,8 @@
-  //Main-----------------Fetch JSON Api Data-------------------------------------------------------------------------------
+//Main-----------------Fetch JSON Api Data-------------------------------------------------------------------------------
 
 
 
-  async function fetchdataStef() {
+async function fetchdataStef() {
     try {
         const response = await fetch("https://kitch41.github.io/Webapps-From-Scratch-23-24/json/info.json");
 
@@ -10,19 +10,21 @@
             throw new Error("Failed to get data");
         }
 
-        const data = await response.json();
+        const stef = await response.json();
 
-        console.log(data);
+
+        return stef;
 
     } catch (error) {
         console.error("Error fetching data:", error);
         throw error; 
     }
 }
+fetchdataStef();
 
-  async function fetchDataNicole() {
+async function fetchDataNicole() {
 
-    const response = await fetch("https://kitch41.github.io/Webapps-From-Scratch-23-24/json/info.json");
+    const response = await fetch("link");
     const data = await response.json();
 
     console.log(data);
@@ -30,17 +32,26 @@
     return data;
 
 }
+// fetchDataNicole();
 
 async function fetchdataStephan() {
+    try {
+        const response = await fetch("https://spacejump3.github.io/web-app-from-scratch-2324/json/info.json");
 
-    const response = await fetch("https://spacejump3.github.io/web-app-from-scratch-2324/app.json");
-    const data = await response.json();
+        if (!response.ok) {
+            throw new Error("Failed to get data for stephan");
+        }
 
-    console.log(data);
+        const data = await response.json();
 
-    return data;
+        return data;
 
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error; 
+    }
 }
+// fetchdataStephan()
 
 async function fetchdataMitchel() {
 
@@ -52,8 +63,28 @@ async function fetchdataMitchel() {
     return data;
 
 }
+
+// fetchdataStephan()
   
   
+async function getData() {
+
+    
+
+    const stef = await fetchdataStef()
+    const stephan = await fetchdataStephan()
+
+    
+    
+    const mdata = {
+        stef,
+        stephan
+    } 
+    
+    console.log(mdata);
+}
+
+getData();
 
 
 //Main-----------------Animal API Data-------------------------------------------------------------------------------
@@ -64,13 +95,12 @@ async function fetchdataMitchel() {
   async function animalapi() {
 
       const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature");
-      const data = await response.json();
+      const animaldata = await response.json();
   
       // console log data from api
-      console.log (data);
+      console.log (animaldata);
 
     }
 
+    animalapi();
 
-    fetchdataStef()
-    fetchdataStephan()
